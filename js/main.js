3aa2c745fb71b00971eb03e313564c8d019dc067
Aman Sharma <aman.sharma@olx.com>
2020-01-06 17:45:06 +0530

    added formSubmit

--- js/main.js
+++ js/main.js
@@ -22,3 +22,40 @@ const sum = function(a){
 sum(1)(2)(3);
 
 
+//XHR Example
+const url = 'json/jsonExample.json';
+const xhr = (function(url){
+    const requestListener = function(){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+    };
+    
+    const requestError = function(error){
+        console.log('We have an issue ', error);
+    };
+    return {
+        send: function(){
+            var request = new XMLHttpRequest();
+            request.onload = requestListener;
+            request.onerror = requestError;
+            request.open('get', url, true);
+            request.send();
+        }
+    };
+})(url);
+
+const fetchWrapper = (function(url){
+    return {
+        fetch: function(){
+            window.fetch(url).then(function(data){
+                console.log(data);
+            }).catch(function(error){
+                console.log('We have an issue ', error);
+            });
+        }
+    };
+})(url);
+
+
+
+
